70d0d6a24073a24540ee08bedcf495166b17126e
Ariel Mints <arikm9@gmail.com>
2020-09-10 01:39:10 +0300

    Remove "22 friends" text from the example component

--- frontend/src/components/Assets/AssetCard.jsx
+++ frontend/src/components/Assets/AssetCard.jsx
@@ -3,25 +3,26 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 
 
-const AssetCard = ({ asset }) => (
-  <Card>
-    <Image src={process.env.PUBLIC_URL + `/img/logos/${asset.name}.svg`} />
-    <Card.Content>
-      <Card.Header>{asset.name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>
-          <Icon disabled name='clock' />
-          {asset.lastUpdated}
-        </span>
-      </Card.Meta>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-)
+const AssetCard = ({ asset }) => {
+  return (
+    <Card>
+      <Image src={process.env.PUBLIC_URL + `/img/logos/${asset.name}.svg`} />
+      <Card.Content>
+        <Card.Header>{asset.name}</Card.Header>
+        <Card.Meta>
+          <span className='date'>
+            <Icon disabled name='clock' />
+            {asset.lastUpdated}
+          </span>
+        </Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+        </a>
+      </Card.Content>
+    </Card>
+  )
+}
 
 export default AssetCard;
