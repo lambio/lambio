b5553ac42b92644c4c649c7fbef10f7db56aa0e8
Ariel Mints <arikm9@gmail.com>
2020-09-10 01:40:24 +0300

    Remove unused imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ComponentsTesterPage from './components/Test/ComponentsTesterPage';
 import AssetCardTester from './components/Assets/AssetCardTester';
 
